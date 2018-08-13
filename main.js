4841e6ab7dd44c7fed589380f08ee326fce29409
LI JIAHAO <lijiahao99131@gmail.com>
2018-08-13 15:29:13 +0800

    docute v3

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
-// window.Docute is exposed by the `docute.js` file
-var doc = new Docute({
-  title: '梨梨机器人', // Default to `<title>` value
-  sidebar: [
-    {
-	  title: '主页',
-      link: '/',	  
-    },
-    {
-      title: '网关',
-      link: '/Gateway',	 
-    },
-    {
-      title: '核心服务程序',
-      link: '/Riri-2',	 
-    }
+docute.init({
+  nav: [
+    {title: '主页', path: '/'},
+    {title: '网关', path: '/Gateway'},
+	{title: '核心程序', path: '/Riri-2'},
   ]
 })
-
-doc.start()
